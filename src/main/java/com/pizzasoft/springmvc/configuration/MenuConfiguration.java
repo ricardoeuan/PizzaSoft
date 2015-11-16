a6dba9ad9278b5b126a211e8477c02d5e7cfd8f7
ricardoeuan <ricardo.euan@hotmail.com>
2015-11-16 10:40:11 -0600

    Web Service Build 1.0

---
+++ src/main/java/com/pizzasoft/springmvc/configuration/MenuConfiguration.java
@@ -0,0 +1,72 @@
+/* Copyright (c) 2015, Ricardo Euán
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the FreeBSD Project.
+ */
+package com.pizzasoft.springmvc.configuration;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.pizzasoft.springmvc.dao.BeverageDAO;
+import com.pizzasoft.springmvc.dao.BeverageDAOImpl;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import org.springframework.jndi.JndiTemplate;
+
+/**
+ *
+ * @author ricardoeuan
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.pizzasoft.springmvc")
+public class MenuConfiguration extends WebMvcConfigurerAdapter {       
+    
+    @Bean
+    public DataSource getDataSource() {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        DataSource dataSource;
+        try {
+            dataSource = (DataSource) jndiTemplate.lookup("java:/SottovoceDS");
+            dataSource.getConnection("root", "");            
+            return dataSource;
+        } catch (NamingException | SQLException ex) {
+            Logger.getLogger(MenuConfiguration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;        
+    }
+    
+    @Bean
+    public BeverageDAO getBeverageDao() {
+        return new BeverageDAOImpl(getDataSource());
+    }        
+}
